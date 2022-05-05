b62f86908e0261a50f9cf195bc7169e0790b8922
pytestauto1 <linussebastion@gmail.com>
2022-05-05 11:14:36 +0100

    Completed module

---
+++ budget/BudgetList.py
@@ -0,0 +1,43 @@
+from . import Expense 
+
+
+
+
+
+class BudgetList:
+    def __init__(self,budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+def main():
+    myBudgetList = BudgetList(1200)
+
+
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+
+    print( myBudgetList.len().str())
+
+if __name__ == '__main__':
+    main()
+
+def append(self,item):
+    if self.sum_expenses & item < self.budget:
+        self.expenses.append(item)
+        self.sum_expenses.append(item)
+    else:
+        self.overages.append(item)
+        self.sum_overages.append(item)
+
+def __len__(self):
+    return self.expenses.sum() + self.overages.sum()
+
+
+
+
