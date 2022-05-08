0830c891f025bdba432ec9634e3e603ce5796a98
pytestauto1 <linussebastion@gmail.com>
2022-05-08 11:29:50 +0100

    5

--- budget/Expense.py
+++ budget/Expense.py
@@ -51,4 +51,5 @@ class Expenses():
 
         unnecessary_expenses = set(self.list - necessary_expenses - food_expenses)
 
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
         
